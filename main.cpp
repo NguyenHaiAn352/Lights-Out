e63ceb4644debd66965b801a6bb4aea5c87c17ba
NguyenHaiAn352 <24021352@vnu.edu.vn>
2025-02-26 13:10:06 +0700

    Core
    Using TicTacToe as the base for Lights Out the game

--- main.cpp
+++ main.cpp
@@ -1,60 +1,13 @@
 #include <iostream>
 #include <SDL.h>
 #include <SDL_image.h>
+#include "defs.h"
+#include "graphics.h"
+#include "logic.h"
 
 using namespace std;
 
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
-const char* WINDOW_TITLE = "Hello World!";
-
-void logErrorAndExit(const char* msg, const char* error)
-{
-    SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "%s: %s", msg, error);
-    SDL_Quit();
-}
-
-SDL_Window* initSDL(int SCREEN_WIDTH, int SCREEN_HEIGHT, const char* WINDOW_TITLE)
-{
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-        logErrorAndExit("SDL_Init", SDL_GetError());
-
-    SDL_Window* window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    //full screen
-    //window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
-    if (window == nullptr) logErrorAndExit("CreateWindow", SDL_GetError());
-
-    if (!IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG))
-        logErrorAndExit( "SDL_image error:", IMG_GetError());
-
-    return window;
-}
-
-SDL_Renderer* createRenderer(SDL_Window* window)
-{
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
-                                              SDL_RENDERER_PRESENTVSYNC);
-    //Khi chạy trong máy ảo (ví dụ phòng máy ở trường)
-    //renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
-
-    if (renderer == nullptr) logErrorAndExit("CreateRenderer", SDL_GetError());
-
-    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
-    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
-
-    return renderer;
-}
-
-void quitSDL(SDL_Window* window, SDL_Renderer* renderer)
-{
-    IMG_Quit();
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-}
-
-void waitUntilKeyPressed()
+void waitUntilKeyPressed() //pause coding until key being pressed
 {
     SDL_Event e;
     while (true) {
@@ -65,50 +18,64 @@ void waitUntilKeyPressed()
     }
 }
 
-void renderTexture(SDL_Texture *texture, int x, int y, SDL_Renderer* renderer)
-{
-	SDL_Rect dest;
-
-	dest.x = x;
-	dest.y = y;
-	SDL_QueryTexture(texture, NULL, NULL, &dest.w, &dest.h);
-
-	SDL_RenderCopy(renderer, texture, NULL, &dest);
+void processClick(int x, int y, Tictactoe& game) {
+    // chuyển tọa độ màn hình x, y thành tọa độ hàng cột của game
+    int clickedCol = (x - BOARD_X) / CELL_SIZE;
+    int clickedRow = (y - BOARD_Y) / CELL_SIZE;
+    game.move(clickedRow, clickedCol);
 }
 
-SDL_Texture *loadTexture(const char *filename, SDL_Renderer* renderer)
-{
-	SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO, "Loading %s", filename);
-
-	SDL_Texture *texture = IMG_LoadTexture(renderer, filename);
-	if (texture == NULL)
-        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Load texture %s", IMG_GetError());
-
-	return texture;
-}
 
 int main(int argc, char *argv[])
 {
-    SDL_Window* window = initSDL(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
-    SDL_Renderer* renderer = createRenderer(window);
+//    Graphics graphics;
+//    graphics.init();
+//
+//    SDL_Texture* background = graphics.loadTexture("images\\whiteStage.png");
+//    graphics.prepareScene(background);
+//
+//    graphics.presentScene();
+//    waitUntilKeyPressed();
+//
+//    SDL_Texture* spongeBob = graphics.loadTexture("Spongebob.png");
+//    graphics.renderTexture(spongeBob, 200, 200);
+//
+//    graphics.presentScene();
+//    waitUntilKeyPressed();
+//
+//    SDL_DestroyTexture( spongeBob );
+//    spongeBob = NULL;
+//    SDL_DestroyTexture( background );
+//    background = NULL;
+//
+//    graphics.quit();
+//    return 0;
+    Graphics graphics;
+    graphics.init();
 
-    SDL_Texture* background = loadTexture("images\\bikiniBottom.jpg", renderer);
-    SDL_RenderCopy( renderer, background, NULL, NULL);
+    Tictactoe game;
+    game.init();
+    graphics.render(game);
 
-    SDL_RenderPresent( renderer );
-    waitUntilKeyPressed();
+    int x, y;
+    SDL_Event event;
+    bool quit = false;
+    while (! quit) {
+        SDL_PollEvent(&event);
+        switch (event.type) {
+            case SDL_QUIT:
+                 quit = true;
+                 break;
+            case SDL_MOUSEBUTTONDOWN:
+                 SDL_GetMouseState(&x, &y);
+                 processClick(x, y, game);
+                 graphics.render(game);
+                 break;
+        }
+        SDL_Delay(100);
+    }
 
-    SDL_Texture* spongeBob = loadTexture("images\\Spongebob.png", renderer);
-    renderTexture(spongeBob, 200, 200, renderer);
-
-    SDL_RenderPresent( renderer );
-    waitUntilKeyPressed();
-
-    SDL_DestroyTexture( spongeBob );
-    spongeBob = NULL;
-    SDL_DestroyTexture( background );
-    background = NULL;
-
-    quitSDL(window, renderer);
+    graphics.quit();
     return 0;
+
 }
