bca84c11c2adde5aa2e2fedab698183aae5eb1a4
NguyenHaiAn352 <24021352@vnu.edu.vn>
2025-04-14 17:22:01 +0700

    Replay Mechanic
    - Further minimize the game's ratio as the goal is to make it looks as clean as possible
    - A simple replay mechanic that receives response via console. Future development will take it directly into the game's surface instead.
    - Leaning towards deprecating SDL_TTF due to constant bugging, and UI will from now on be done via UIDesign.fla (animation executable)

--- defs.h
+++ defs.h
@@ -1,13 +1,13 @@
 #ifndef _DEFS__H
 #define _DEFS__H
 
-const int SCREEN_WIDTH = 600;
-const int SCREEN_HEIGHT = 800;
+const int SCREEN_WIDTH = 260;
+const int SCREEN_HEIGHT = 350;
 const char* WINDOW_TITLE = "Lights Out!";
 
-#define BOARD_X 10
-#define BOARD_Y 10
-#define CELL_SIZE 80
+const int BOARD_X = 10;
+const int BOARD_Y = 10;
+const int CELL_SIZE = 80;
 
 #endif
 
--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 #include <set>
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_ttf.h>
 #include "defs.h"
 #include "graphics.h"
 #include "logic.h"
-#include "additional.h"
 
 using namespace std;
 
@@ -23,55 +23,67 @@ void waitUntilKeyPressed() //pause coding until key being pressed
 }
 
 void processClick(int x, int y, Lightsout& game) {
-    if (x > 10 && x < 10 + 240 && y > 10*2 + CELL_SIZE*BOARD_SIZE && y < 10*2 + CELL_SIZE*BOARD_SIZE + 80) {
-        game.giveHint();
+    bool beatenTheGame = true;
+    for (int i = 0; i < BOARD_SIZE*BOARD_SIZE; i++){
+        if (game.CLICK_STATE[i] != 0) beatenTheGame = false;
     }
-    else {
-    // chuyển tọa độ màn hình x, y thành tọa độ hàng cột của game
-    int clickedCol = (x - BOARD_X) / CELL_SIZE;
-    int clickedRow = (y - BOARD_Y) / CELL_SIZE;
-    game.move(clickedRow, clickedCol);
+    if (beatenTheGame) {
+            game.congratulation();
+
+    } else {
+        if (x > 10 && x < 10 + 240 && y > 10*2 + CELL_SIZE*BOARD_SIZE && y < 10*2 + CELL_SIZE*BOARD_SIZE + 80) {
+            game.giveHint();
+        }
+        else {
+            int clickedCol = (x - BOARD_X) / CELL_SIZE;
+            int clickedRow = (y - BOARD_Y) / CELL_SIZE;
+            game.move(clickedRow, clickedCol);
+        }
     }
 }
 
+bool askReplay() {
+    cout << "Play again? (y/n): ";
+    char choice;
+    cin >> choice;
+    return choice == 'y';
+}
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
+	// Initialization
+	Graphics graphics;
+   	graphics.init();
+    	Lightsout game;
+    	game.init();
+    	game.gameInit();
+    	graphics.render(game);
 
-//    int input;
-//    cout << "Select your board size: 3   4   5";
-//    cin >> input;
-//
-//    int BOARD_SIZE = input;
-
-    Graphics graphics;
-    graphics.init();
-
-    Lightsout game;
-    game.init();
-    game.gameInit();
-
-    graphics.render(game);
-
-    int x, y;
-    SDL_Event event;
-    bool quit = false;
-    while (! quit) {
-        SDL_PollEvent(&event);
-        switch (event.type) {
-            case SDL_QUIT:
-                 quit = true;
-                 break;
-            case SDL_MOUSEBUTTONDOWN:
-                 SDL_GetMouseState(&x, &y);
-                 processClick(x, y, game);
-                 graphics.render(game);
-                 break;
-        }
-        SDL_Delay(50);
-    }
-
-    graphics.quit();
-    return 0;
-
+	bool quit = false;
+	int x, y;
+	SDL_Event event;
+	while (!quit) {
+		SDL_PollEvent(&event);
+        	switch (event.type) {
+            		case SDL_QUIT:
+                 		quit == true;
+                 		break;
+            		case SDL_MOUSEBUTTONDOWN:
+                 		SDL_GetMouseState(&x, &y);
+                 		processClick(x, y, game);
+                 		if (game.haveWon){
+                            graphics.render(game);
+                            if (askReplay()){
+                                game.haveWon = false;
+                                game.reset();
+                                game.init();
+                                game.gameInit();
+                            } else {quit == true;}
+                		}
+                		graphics.render(game);
+                break;
+		}
+        	SDL_Delay(50);
+    	}
+   	graphics.quit();
+	return 0;
 }
